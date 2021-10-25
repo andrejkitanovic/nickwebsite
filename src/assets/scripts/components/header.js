9656af16941d83ec12aaf4171c078b3c612dba92
andrejkitanovic <kitanovicandrej213@gmail.com>
2021-10-25 19:41:59 +0200

    liquidspace final

--- src/assets/scripts/components/header.js
+++ src/assets/scripts/components/header.js
@@ -6,21 +6,21 @@ if (gsap) {
     $(function () {
         const header = $('header');
 
-        const loseState = () => {
-            $(header).map(function () {
-                $(this).addClass('hidden');
-            });
-        };
+        // const loseState = () => {
+        //     $(header).map(function () {
+        //         $(this).addClass('hidden');
+        //     });
+        // };
 
-        ScrollTrigger.create({
-            start: 'top top',
-            end: document.innerHeight,
-            onUpdate: (self) => {
-                self.direction === -1
-                    ? $(header).removeClass('hidden')
-                    : loseState();
-            },
-        });
+        // ScrollTrigger.create({
+        //     start: 'top top',
+        //     end: document.innerHeight,
+        //     onUpdate: (self) => {
+        //         self.direction === -1
+        //             ? $(header).removeClass('hidden')
+        //             : loseState();
+        //     },
+        // });
 
         const linkWithChildren = $(header).find('.has-children');
 
